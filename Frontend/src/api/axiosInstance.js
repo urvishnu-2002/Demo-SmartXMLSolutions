74fe51f56fc79f5c6ce298c2821d29efb0bcc5f0
urvishnu-2002 <i.m.vishnuvardhan2002@gmail.com>
2026-01-21 14:12:28 +0530

    .,.,.,.,

--- Frontend/src/api/axiosInstance.js
+++ Frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "https://demo-smartxmlsolutions.onrender.com/api/",
+    baseURL: "https://demo-smartxmlsolutions-backend.onrender.com/api/",
     headers: {
         "Content-Type": "application/json",
     },
